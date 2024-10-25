d8cf839c08993f0d4dfb03aff93f01826e719628
Bertan Günyel <bertan.gunyel@gmail.com>
2024-10-25 15:29:08 +0300

    Experiment: Backbone weights frozen

--- source/ml/models/sentiment/model.py
+++ source/ml/models/sentiment/model.py
@@ -25,6 +25,20 @@ class SentimentModel(nn.Module):
 
         # self.out_fc = nn.Linear(self.backbone_hidden_size, num_classes)
 
+        # freeze base model parameters
+        for name, param in self.backbone.named_parameters():
+            param.requires_grad = False
+
+        dummy = -32
+
+        """
+        # unfreeze base model pooling layers
+        for name, param in model.base_model.named_parameters():
+            if "pooler" in name:
+                param.requires_grad = True
+        """
+
+
     def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor):
         backbone_out = self.backbone(input_ids=input_ids, attention_mask=attention_mask)
         x = backbone_out.last_hidden_state[:, 0, :]
